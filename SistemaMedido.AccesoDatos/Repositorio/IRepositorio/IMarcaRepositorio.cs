3c64d07e204a1ccce99a22c607dc39f34d5ddb86
Daniel Flores <vflorestrejo@gmail.com>
2024-04-10 19:24:29 -0600

    Add Modulo Marca Backend

---
+++ SistemaMedido.AccesoDatos/Repositorio/IRepositorio/IMarcaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaMedico.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMedido.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IMarcaRepositorio : IRepositorio<Marca>
+    {
+        void Actualizar(Marca marca);
+    }
+}
