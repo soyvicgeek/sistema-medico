71df01c3df05db5744cf8b870ef4b2c6758a099b
Daniel Flores <vflorestrejo@gmail.com>
2024-04-10 19:13:12 -0600

    Add Modulo Categoria Frontend

---
+++ SistemaMedico/Areas/Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaMedico.Modelos;
+using SistemaMedido.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaMedico.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public CategoriaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Método Upsert GET
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Categoria categoria = new Categoria();
+            if (id == null)
+            {
+                //creamos un nuevo registro
+                categoria.Estado = true;
+                return View(categoria);
+            }
+
+            categoria = await _unidadTrabajo.Categoria.Obtener(id.GetValueOrDefault());
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                if (categoria.Id == 0)
+                {
+                    await _unidadTrabajo.Categoria.Agregar(categoria);
+                }
+                else
+                {
+                    _unidadTrabajo.Categoria.Actualizar(categoria);
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoriaDB = await _unidadTrabajo.Categoria.Obtener(id);
+            if (categoriaDB == null)
+            {
+                return Json(new { success = false, message = "Error al borrar el registro en la base de datos" });
+            }
+            _unidadTrabajo.Categoria.Remover(categoriaDB);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Categoria eliminada con exito" });
+        }
+
+        // Región API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Categoria.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Categoria.ObtenerTodos();
+
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim()
+                                    == nombre.ToLower().Trim()
+                                    && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+        // End Región
+    }
+}
