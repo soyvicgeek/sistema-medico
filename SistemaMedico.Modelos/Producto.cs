dc2357bfc24ba8f8da9bc4997b87b6006d4f762f
Daniel Flores <vflorestrejo@gmail.com>
2024-04-15 23:49:33 -0600

    Add Producto Relations

---
+++ SistemaMedico.Modelos/Producto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMedico.Modelos
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Número de serie es requerido")]
+        [MaxLength(30, ErrorMessage = "Máximo 30 Caractéres")]
+        public string NumeroSerie { get; set; }
+        [Required(ErrorMessage = "La descripción es requereida")]
+        [MaxLength(100, ErrorMessage = "Máximo 100 Caractéres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Precio Requerido")]
+        public double Precio { get; set; }
+        [Required(ErrorMessage = "El costo es Requerido")]
+        public double Costo { get; set; }
+        public string ImagenUrl { get; set; }
+        [Required(ErrorMessage = "El Estado es Requerdio")]
+        public bool Estado { get; set; }
+
+        //Hagamos la relación con la tabla categoría
+        [Required(ErrorMessage = "La categpría es requerida")]
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        //Hagamos la relación en la tabla Marca
+        [Required(ErrorMessage = "La Marca es Requerida")]
+        public int MarcaId { get; set; }
+        [ForeignKey("MarcaId")]
+        public Marca Marca { get; set; }
+
+        //Hagamos la recursividad del padre
+        public int? PadreId { get; set; }
+        public virtual Producto Padre { get; set; }
+    }
+}
--- SistemaMedido.AccesoDatos/Migrations/20240416054824_AgregarProductoMigracion.Designer.cs
+++ SistemaMedido.AccesoDatos/Migrations/20240416054824_AgregarProductoMigracion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaMedico.AccesoDatos.Data;
 
@@ -11,9 +12,11 @@ using SistemaMedico.AccesoDatos.Data;
 namespace SistemaMedido.AccesoDatos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240416054824_AgregarProductoMigracion")]
+    partial class AgregarProductoMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
